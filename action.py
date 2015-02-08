222138dc860fad11cb09746ff7a1c9d8a0f6877a
Rikki Guy <git@rikkiguy.me.uk>
2015-02-08 20:48:27 +0000

    New Command-based implementation of Action

--- action.py
+++ action.py
@@ -1,94 +1,179 @@
 # movement.py
 import math
 
-class Action():
-    """Provides an interface to movement and other robot actions"""
 
-    Debug = False # set Action.Debug = True in your application to enable
+# Don't create these directly, instead use Action.xyz()
+class Command():
+    """A simple command wrapper class"""
+    @classmethod
+    def fromCommandString(cls, commandString):
+        parts = commandString.split(" ")
+        return cls(parts[0], parts[1:])
+    
+    def __init__(self, command, args=[]):
+        self.command = command
+        self.args = args
+        
+        if not self.isValid():
+            raise Exception("Attempt to create invalid command" + self)
+        
+    def getCommandString():
+        return self.command + " " + (" ".join(self.args))
+    
+    def isValid(self):
+        return (self.command in ["RUN", "TURN", "LED", "KICK", "CATCH", "MPU"])
+    
+    def __unicode__(self):
+        return self.getCommandString()
+    
+    def __eq__(self, other):
+        return (self.command == other.command) and (self.args == other.args)
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+class MpuCommand(Command):
+    """Wrapper around MPU commands"""
+    def __init__(self, subcmd, args=[]):
+        super(MpuCommand, self).__init__("MPU", [subcmd] + args)
+        
+    def isValid(self):
+        return (super(MpuCommand, self).isValid()) and
+               (self.args[0] in ["INIT", "SETHOME", "GETYAW", "STABL", "CALIB"])
+
+
+class Commander():
+    """Deals directly with the robot, to cater for all your robot commanding needs"""
+    def __init__(self, comm):
+        self.comm = comm
+        
+    def executeCommandList(self, commands):            
+        """Takes a list of Command objects, and executes them in order"""
+        commands = Commander._optimiseCommandList(commands)
+        results = map(lambda cmd: self.executeCommand(cmd), commands)
+        return results
+        
+    def executeCommand(self, command):
+        """Execute a single command"""
+        commandString = command.getCommandString()
+        comm.write(commandString + "\r\n")
+        comm.flush()
+        
+        success = False
+        result = []
+        # wait for output
+        while True:
+            line = comm.readline().rstrip()
+            
+            if(line == "DONE"):
+                success = True
+                break
+            elif(line == "FAILED"):
+                success = False
+                break
+            
+            result.append(line)
+            
+        if success:
+            return (command, result)
+        
+        else:
+            raise Exception("Command execution failed: " + cmd + " With result: " + " ".join(result))
+                
+    
+    @staticmethod
+    def _optimiseCommandList(commands):
+        """Simple optimisation on the list of commands"""
+        
+        commands_to_keep = []
+        
+        # Filter out all but the last run command
+        has_run = False
+        for cmd in reversed(commands):
+            
+            if(cmd.command == "RUN"):
+                if(has_run):
+                    continue
+                has_run = True
+                
+            commands_to_keep.append(cmd)
+            
+        return commands_to_keep[::-1]
+        
+
+class Action():
+    """Higher-level robot activities"""
     
     # Motor angles, starting from the front left, going anticlockwise
     # MOTOR_ANGLES = [ math.pi/3.0, math.pi, math.pi*5.0/3.0 ]
     MOTOR_ANGLES = [ math.pi*5.0/6.0, math.pi*3.0/2.0, math.pi/6.0 ]
     MOTORS = [ (math.cos(angle), math.sin(angle)) for angle in MOTOR_ANGLES ]
     
-    def __init__(self, comm):
-        self.comm = comm
-    
     # Public methods #
     
     # Movement
-    def move(self, angle, scale):
+    @classmethod
+    def move(cls, angle, scale):
         """Move the robot in the angle (radians) from the front, with speed -1 to +1"""
-        motor_speeds = [ Action._calc_motor_speed(motor, angle) for motor in self.MOTORS ]
-        motor_speeds = Action._normalise_speeds(motor_speeds)
+        motor_speeds = [ cls._calc_motor_speed(motor, angle) for motor in cls.MOTORS ]
+        motor_speeds = cls._normalise_speeds(motor_speeds)
         motor_speeds = map(lambda x: x*scale, motor_speeds)
-        motor_speeds = map(Action._percentage_speed, motor_speeds)
+        motor_speeds = map(cls._percentage_speed, motor_speeds)
         
-        self._send_run(motor_speeds)
+        return [ Command("RUN", motor_speeds) ]
   
-    def turn(self, speed):
+    @classmethod
+    def turn(cls, speed):
         """Turn the robot in the given direction, clockwise/anticlockwise"""
         speed = int(speed)
-        self._send_run([-speed, speed, speed])
+        return [ Command("RUN", [speed, speed, -speed]) ]
   
-    def stop(self):
-        motor_speeds = [ 0.0, 0.0, 0.0 ]
-        self._send_run(motor_speeds)
-  
+    @classmethod
+    def stop(cls):
+        """Stooooppp!!"""
+        return [ Command("RUN", []) ]
+
     # Kicking
-    def kick(self, scale=100):
-        self._send_command("KICK",[scale])
+    @clasmethod
+    def kick(cls, scale=100):
+        """Kick the kicker"""
+        return [ Command("KICK", [scale]) ]
     
-    def catch(self, scale=100):
-        self._send_command("CATCH",[scale])
+    @classmethod
+    def catch(cls, scale=100):
+        """Catch the ball (hopefully)"""
+        return [ Command("CATCH", [scale]) ]
 
     # Utility
-    def ping(self):
-        self._send_command("PING", [])
-    
+    @classmethod
+    def initialiseMpu(cls, current_rotation=0.0):
+        """Setup the gyroscope - don't move the robot!"""
+        return [ MpuCommand("INIT"),
+                 MpuCommand("STABL"),
+                 MpuCommand("SETHOME", [current_rotation]) ]
+
+    @classmethod
+    def ping(cls):
+        """Make sure the robot is still alive"""    
+        return [ Command("PING") ]
     
     # Private Methods #
-    
-    # Commands
-    def _send_run(self, speeds):
-        self._send_command("RUN", speeds)
-    
-    # TODO: decide on the kick protocol etc.
-  
+      
     # Utility
-    @staticmethod
-    def _normalise_speeds(speeds):
+    @classmethod
+    def _normalise_speeds(cls, speeds):
         maxspeed = max(speeds)
         return map( lambda x: (x/maxspeed)*x, speeds )        
     
-    @staticmethod
-    def _percentage_speed(speed):
+    @classmethod
+    def _percentage_speed(cls, speed):
         # We need the speed to be an integer between 100 and -100
         speed = int(round(100.0 * speed))
         speed = max(-100, min(speed, 100))
         return speed
     
-    @staticmethod
-    def _calc_motor_speed(motor, angle):
+    @classmethod
+    def _calc_motor_speed(cls, motor, angle):
         return (math.cos(angle) * motor[0] - math.sin(angle) * motor[1])
-    
-    @staticmethod
-    def _get_command_string(command, args):
-        commstr = str(command)
-        
-        for arg in args:
-            commstr = commstr + " " + str(arg)
-            
-        commstr = commstr + "\r\n"            
-        return commstr
-    
-    def _send_command(self, command, args):
-        commstr = Action._get_command_string(command, args)
-        if self.Debug:
-            print "Sending command: " + commstr
-        if self.comm is not None:
-            self.comm.write(commstr)
-            self.comm.flush()
-    
     
     
