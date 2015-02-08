b82605db53fe744cd5f1f303fc38326e018c84c4
Robert <s1223255@sms.ed.ac.uk>
2015-02-08 13:34:41 +0000

    The sides are probably wrong limited to 1 occurance

--- planning/models.py
+++ planning/models.py
@@ -357,6 +357,8 @@ class World(object):
         self._goals.append(Goal(0, 0, self._pitch.height/2.0, 0))
         self._goals.append(Goal(3, self._pitch.width, self._pitch.height/2.0, pi))
 
+        self._allowWarning = True
+
     @property
     def our_attacker(self):
         return self._robots[2] if self._our_side == 'left' else self._robots[1]
@@ -399,9 +401,12 @@ class World(object):
         self.ball.vector = pos_dict['ball']
         # Checking if the robot locations make sense:
         # Is the side correct:
-        if (self._our_side == 'left' and not(self.our_defender.x < self.their_attacker.x
-            < self.our_attacker.x < self.their_defender.x)):
-            print "WARNING: The sides are probably wrong!"
-        if (self._our_side == 'right' and not(self.our_defender.x > self.their_attacker.x
-            > self.our_attacker.x > self.their_defender.x)):
-            print "WARNING: The sides are probably wrong!"
+        if self._allowWarning:
+            if (self._our_side == 'left' and not(self.our_defender.x < self.their_attacker.x
+                < self.our_attacker.x < self.their_defender.x)):
+                print "WARNING: The sides are probably wrong!"
+                self._allowWarning = False
+            if (self._our_side == 'right' and not(self.our_defender.x > self.their_attacker.x
+                > self.our_attacker.x > self.their_defender.x)):
+                print "WARNING: The sides are probably wrong!"
+                self._allowWarning = False
