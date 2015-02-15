3e55cb94a08d22e59b85d44a8e89efd95d99fb33
Rikki Guy <git@rikkiguy.me.uk>
2015-02-15 16:12:32 +0000

    Remove class variables from control.action.*

--- control/action.py
+++ control/action.py
@@ -7,55 +7,55 @@ MOTOR_ANGLES = [ math.pi*5.0/6.0, math.pi*3.0/2.0, math.pi/6.0 ]
 MOTORS = [ (math.cos(angle), math.sin(angle)) for angle in MOTOR_ANGLES ]
 
 # Utilities
-def _normalise_speeds(cls, speeds):
+def _normalise_speeds(speeds):
     maxspeed = max(speeds)
     return map( lambda x: (x/maxspeed)*x, speeds )        
 
-def _percentage_speed(cls, speed):
+def _percentage_speed(speed):
     # We need the speed to be an integer between 100 and -100
     speed = int(round(100.0 * speed))
     speed = max(-100, min(speed, 100))
     return speed
 
-def _calc_motor_speed(cls, motor, angle):
+def _calc_motor_speed(motor, angle):
     return (math.cos(angle) * motor[0] - math.sin(angle) * motor[1])
     
     
 # Actions
-def move(cls, angle, scale):
+def move(angle, scale):
     """Move the robot in the angle (radians) from the front, with speed -1 to +1"""
-    motor_speeds = [ cls._calc_motor_speed(motor, angle) for motor in cls.MOTORS ]
-    motor_speeds = cls._normalise_speeds(motor_speeds)
+    motor_speeds = [ _calc_motor_speed(motor, angle) for motor in MOTORS ]
+    motor_speeds = _normalise_speeds(motor_speeds)
     motor_speeds = map(lambda x: x*scale, motor_speeds)
-    motor_speeds = map(lambda x: cls._percentage_speed(x), motor_speeds)
+    motor_speeds = map(lambda x: _percentage_speed(x), motor_speeds)
     
     return [ Command("RUN", motor_speeds) ]
 
-def turn(cls, speed):
+def turn(speed):
     """Turn the robot in the given direction, clockwise/anticlockwise"""
     speed = int(speed)
     return [ Command("RUN", [speed, speed, -speed]) ]
 
-def stop(cls):
+def stop():
     """Stooooppp!!"""
     return [ Command("RUN", []) ]
 
 # Kicking
-def kick(cls, scale=100):
+def kick(scale=100):
     """Kick the kicker"""
     return [ Command("KICK", [scale]) ]
 
-def catch(cls, scale=100):
+def catch(scale=100):
     """Catch the ball (hopefully)"""
     return [ Command("CATCH", [scale]) ]
 
 # Utility
-def initialiseMpu(cls, current_rotation=0.0):
+def initialiseMpu(current_rotation=0.0):
     """Setup the gyroscope - don't move the robot!"""
     return [ MpuCommand("INIT"),
                 MpuCommand("STABL"),
                 MpuCommand("SETHOME", [current_rotation]) ]
 
-def ping(cls):
+def ping():
     """Make sure the robot is still alive"""    
     return [ Command("PING") ]
