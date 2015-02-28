e86b4c6cbe9abfffb0bd5cabbac44f4fb171bcc0
Rikki Guy <git@rikkiguy.me.uk>
2015-02-28 23:37:22 +0000

    Added state variables for commands

--- Arduino/Movement/src/command.cpp
+++ Arduino/Movement/src/command.cpp
@@ -18,9 +18,23 @@
  *  <MoveCmdSpd> = speed argument. Scale from 0 to 100. Integer.
  *  <MoveCmd>    = One of M for Move, T for Turn, or S for Stop.
  *  <KickCmd>    = One of K for Kick, or I for idle.
- *  <CatchCmd>   = One of C for Catch, or R for Release.
+ *  <CatchCmd>   = One of C for Catch, R for Release, or I for Idle.
  *  <CurrentAngle> = Current angle we believe the robot is facing.
  */
+
+// Command IDs
+static unsigned long movement_command_id = 0L;
+static unsigned long kicker_command_id = 0L;
+static unsigned long catcher_command_id = 0L;
+
+// Commands being executed
+static char movement_command = 'S';
+static char kicker_command = 'I';
+static char catcher_command = 'C';
+
+// Movement command args
+static float movement_direction = 0.0f;
+static int movement_speed = 0;
 
 void setup_command(void)
 {
