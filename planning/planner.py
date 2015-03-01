6d9add1bdda937045c3a28894c8f9ad2ebd6979c
Thomas Cumming <thomas.cumming18@gmail.com>
2015-03-01 19:37:15 +0000

    Fixed a calibration issue

--- planning/planner.py
+++ planning/planner.py
@@ -37,6 +37,8 @@ class Planner:
         ball = self._world.ball
         our_attacker = self._world.our_attacker
         our_defender = self._world.our_defender
+        their_attacker = self._world.their_attacker
+        their_defender = self._world.their_defender
 
         world = self._world
         role = self._role
@@ -62,19 +64,23 @@ class Planner:
                 if not robot.has_ball(ball):
                     self._current_task = AcquireBall(world, robot, role)
                     self._current_task.execute()
+
                 else:
-                    (pass_x_dest, pass_y_dest) = TODO
+                    (pass_x_dest, pass_y_dest) = (clear_pathX, clear_pathY)
                     self._current_state = MOVING_TO_PT_STATE
-                    self._current_task = MoveToPoint(world, robot, role, X, Y)
+                    self._current_task = MoveToPoint(world, robot, role, pass_x_dest, pass_y_dest)
                     self.execute()
+
             elif state == MOVING_TO_PT_STATE:
                 if abs(robot.get_displacement_to_point(self._current_task.x, self._current_task.y)) > 30:
                     self._current_task.execute()
+
                 else:
                     self._current_state = IDLE_STATE
                     sel._current_task = None
+
             elif state == IDLE_STATE:
-                if our_defender.y == our_attacker.y:
+                if abs(robot.get_displacement_to_point(our_defender.y, our_attacker.y)) > 30:
                     self._current_state = PASSING_STATE
                     if role == 'attacker':
                         self._current_task = KickToPoint(world, robot, role, our_defender.x, our_defender.y)    
@@ -83,9 +89,11 @@ class Planner:
                     self._current_task.execute()
                 else:
                     return
+
             elif state == PASSING_STATE:
                 if not self._current_task.kicked:
                     self._current_task.execute()
+
                 else:
                     self._current_state = INITIAL_STATE
                     self._current_task = None   
@@ -97,21 +105,29 @@ class Planner:
                 elif role == 'defender':
                     self._current_task = MoveToPoint(world, robot, role, robot.x, our_attacker.y)
                 self._current_task.execute()
+
             elif state == MOVING_TO_PT_STATE:
-                if not our_zone.isInside(ball.x, ball.y):
+                ## Should we use prediction instead since this way is probably too slow in term of reaction time?
+                ## Violates the passing protocol.
+                ## Perhaps is unnecessary work and should just leave as is - receiving robot doesn't *need* to actually
+                ## catch the ball 
+                if not our_zone.isInside(ball.x, ball.y):   
                     if role == 'attacker':
                         self._current_task = MoveToPoint(world, robot, role, robot.x, our_defender.y)
                     elif role == 'defender':
                         self._current_task = MoveToPoint(world, robot, role, robot.x, our_attacker.y)
                     self._current_task.execute()
+
                 else:
                     self._current_state = ACQUIRING_BALL_STATE
                     self._current_task = AcquireBall(world, robot, role)
                     self._current_task.execute()
+
             elif state == ACQUIRING_BALL_STATE:
                 if not robot.has_ball(ball):
                     self._current_task = AcquireBall(world, robot, role)
                     self._current_task.execute()
+
                 else:
                     self._current_state = INITIAL_STATE
                     self._current_task = None
