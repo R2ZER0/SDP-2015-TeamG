1e26c352959ff9bde8aa42e944e0498411613e3c
Rikki Guy <git@rikkiguy.me.uk>
2015-03-01 13:38:37 +0000

    Added service/setup for movement

--- Arduino/Movement/src/movement.cpp
+++ Arduino/Movement/src/movement.cpp
@@ -1,17 +1,27 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Movement
 ////////////////////////////////////////////////////////////////////////////////
-
+#include "movement.h"
 #include "Angle.h"
+#include <Arduino.h>
+#include <SDPArdino.h>
+#include "config.h"
+#include "command.h"
 
+int current_command = MOVEMENT_COMMAND_STOP;
 Angle targetAngle();
+
+void on_new_command(int cmd, float dir, int spd)
+{
+    // TODO
+}
 
 void setup_movement()
 {
-    
+    stopAllMotors();
 }
 
 void service_movement()
 {
-    
+    command_sethook_movement(&on_new_command);
 }
