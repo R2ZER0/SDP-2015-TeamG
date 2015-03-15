ce4326f54e3875391823fccbb56207ea6ba3fbb7
Rikki Guy <git@rikkiguy.me.uk>
2015-03-15 18:14:49 +0000

    Allow getting serial port from commandline in action_console.py

--- scripts/action_console.py
+++ scripts/action_console.py
@@ -1,10 +1,17 @@
 from action import Action
 from serial import Serial
 from math import pi
+import sys
 
-s = Serial('/dev/ttyACM0', 115200)
+serial_port = "/dev/ttyACM0"
+if len(sys.argv) > 1:
+    serial_port = sys.argv[1]
+    print "Using serial port " + serial_port
+        
+
+s = Serial(serial_port, 115200)
 a = Action(s)
 
 def quit():
-  a.exit()
-  exit()
+      a.exit()
+      exit()
