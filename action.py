bbddda9ef70dffb3518dfe49d7db5b6bef9c5fbc
Rikki Guy <git@rikkiguy.me.uk>
2015-03-15 18:57:28 +0000

    Added SpeedTest sketch to show encoder values
    Conflicts:
    	action.py

--- action.py
+++ action.py
@@ -136,92 +136,93 @@ class Action():
 		self.move_handle = MovementActionHandle(self.move_handle.idx+1, cmd, angle, scale)
 		return self.move_handle
 
-	def move(self, angle, scale=64):
-		return self._cmd_movement('M', angle+math.pi/2, scale)
-		
-	def turnBy(self, angle, scale=64):
-		target = mkangle(self.curr_dir + angle)
-		#print "Turning from " + str(self.curr_dir) + " to " + str(target)
-		return self._cmd_movement('T', target, scale)
-		
-	def stop(self):
-		return self._cmd_movement('S', 0, 0)
-		
-	 # Kicker command
-	def kick(self, scale=100):
-		self.kick_handle._onNextCommand()
-		self.kick_handle = KickerActionHandle(self.kick_handle.idx+1, 'K', scale)
-		return self.kick_handle
-	
-	# Catcher commands
-	def _cmd_catcher(self, cmd, scale):
-		self.catch_handle._onNextCommand()
-		self.catch_handle = CatcherActionHandle(self.catch_handle.idx+1, cmd, scale)
-		return self.catch_handle
-	
-	def catch(self, scale=100):
-		return self._cmd_catcher('C', scale)
-	
-	def open_catcher(self, scale=100):
-		return self._cmd_catcher('R', scale)
-	
-	# State processing
-	def process_message(self, message):
-		"""Parse and process the state message from the arduino"""
-		#'\((\d+) ([SMT]) (-?\d+) (1|0) (\d+) ([KI]) (1|0) (\d+) ([CRI]) (1|0) (-?\d+)\)'
-		#    mID   mCMD   mDIR    mFIN   kID   kCMD   kFIN   cID   cCMD   cFIN  sDIR
-		#     1      2      3       4     5     6       7     8      9     10    11
-		res = Action.msg_re.match(message)
-		
-		if res is not None:
-			#print "Got msg " + str(self.num_messages_recvd)
-			self.num_messages_recvd += 1
-			
-			move_id = int(res.group(1))
-			move_cmd = res.group(2)
-			move_dir = i2f(int(res.group(3)))
-			move_fin = (1 == int(res.group(4)))
-			
-			kick_id = int(res.group(5))
-			kick_cmd = res.group(6)
-			kick_fin = (1 == int(res.group(7)))
-			
-			catch_id = int(res.group(8))
-			catch_cmd = res.group(9)
-			catch_fin = (1 == int(res.group(10)))
-			
-			state_dir = i2f(int(res.group(11)))
-			self.curr_dir = state_dir
-			
-			# Detect if the command is running, finished etc
-			if move_id == self.move_handle.idx:
-				if not self.move_handle.running:
-					self.move_handle._onRunning()
-					
-				if move_fin and not self.move_handle.finished:
-					self.move_handle._onComplete()
-			
-			if kick_id == self.kick_handle.idx:
-				if not self.kick_handle.running:
-					self.kick_handle._onRunning()
-					
-				if kick_fin and not self.kick_handle.finished:
-					self.kick_handle._onComplete()
-					
-			if catch_id == self.catch_handle.idx:
-				if not self.catch_handle.running:
-					self.catch_handle._onRunning()
-					
-				if catch_fin and not self.catch_handle.finished:
-					self.catch_handle._onComplete()
-		else:
-			#print "# " + message
-			pass
-		
-		
-	def run_state_processor(self):
-		"""Processes incoming state messages"""
-		while not self._exit:
+
+    def move(self, angle, scale=64):
+        return self._cmd_movement('M', angle+math.pi/2, scale)
+        
+    def turnBy(self, angle, scale=64):
+        target = mkangle(self.curr_dir + angle)
+        #print "Turning from " + str(self.curr_dir) + " to " + str(target)
+        return self._cmd_movement('T', target, scale)
+        
+    def stop(self):
+        return self._cmd_movement('S', 0, 0)
+        
+     # Kicker command
+    def kick(self, scale=100):
+        self.kick_handle._onNextCommand()
+        self.kick_handle = KickerActionHandle(self.kick_handle.idx+1, 'K', scale)
+        return self.kick_handle
+    
+    # Catcher commands
+    def _cmd_catcher(self, cmd, scale):
+        self.catch_handle._onNextCommand()
+        self.catch_handle = CatcherActionHandle(self.catch_handle.idx+1, cmd, scale)
+        return self.catch_handle
+    
+    def catch(self, scale=100):
+        return self._cmd_catcher('C', scale)
+    
+    def open_catcher(self, scale=100):
+        return self._cmd_catcher('R', scale)
+    
+    # State processing
+    def process_message(self, message):
+        """Parse and process the state message from the arduino"""
+        #'\((\d+) ([SMT]) (-?\d+) (1|0) (\d+) ([KI]) (1|0) (\d+) ([CRI]) (1|0) (-?\d+)\)'
+        #    mID   mCMD   mDIR    mFIN   kID   kCMD   kFIN   cID   cCMD   cFIN  sDIR
+        #     1      2      3       4     5     6       7     8      9     10    11
+        res = Action.msg_re.match(message)
+        
+        if res is not None:
+            #print "Got msg " + str(self.num_messages_recvd)
+            self.num_messages_recvd += 1
+            
+            move_id = int(res.group(1))
+            move_cmd = res.group(2)
+            move_dir = i2f(int(res.group(3)))
+            move_fin = (1 == int(res.group(4)))
+            
+            kick_id = int(res.group(5))
+            kick_cmd = res.group(6)
+            kick_fin = (1 == int(res.group(7)))
+            
+            catch_id = int(res.group(8))
+            catch_cmd = res.group(9)
+            catch_fin = (1 == int(res.group(10)))
+            
+            state_dir = i2f(int(res.group(11)))
+            self.curr_dir = state_dir
+            
+            # Detect if the command is running, finished etc
+            if move_id == self.move_handle.idx:
+                if not self.move_handle.running:
+                    self.move_handle._onRunning()
+                    
+                if move_fin and not self.move_handle.finished:
+                    self.move_handle._onComplete()
+            
+            if kick_id == self.kick_handle.idx:
+                if not self.kick_handle.running:
+                    self.kick_handle._onRunning()
+                    
+                if kick_fin and not self.kick_handle.finished:
+                    self.kick_handle._onComplete()
+                    
+            if catch_id == self.catch_handle.idx:
+                if not self.catch_handle.running:
+                    self.catch_handle._onRunning()
+                    
+                if catch_fin and not self.catch_handle.finished:
+                    self.catch_handle._onComplete()
+        else:
+            print "# " + message
+            pass
+        
+        
+    def run_state_processor(self):
+        """Processes incoming state messages"""
+        while not self._exit:
 	
 			self.comm.timeout = 0.1
 			line = self.comm.readline()
