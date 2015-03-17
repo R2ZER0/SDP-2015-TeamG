cf490d3e1cbc1829d5e03419bf2492b8bed7c863
Rikki Guy <git@rikkiguy.me.uk>
2015-03-17 15:31:00 +0000

    Initial command.cpp/h for group10

---
+++ Arduino/10/Receiver/command.h
@@ -0,0 +1,29 @@
+/* command.h */
+#ifndef _COMMAND_H_
+#define _COMMAND_H_
+
+// FIXME: Redefine these
+#define MOVEMENT_COMMAND_MOVE 'M'
+#define MOVEMENT_COMMAND_TURN 'T'
+#define MOVEMENT_COMMAND_STOP 'S'
+
+#define KICKER_COMMAND_KICK 'K'
+#define KICKER_COMMAND_IDLE 'I'
+
+#define CATCHER_COMMAND_CATCH   'C'
+#define CATCHER_COMMAND_RELEASE 'R'
+#define CATCHER_COMMAND_IDLE    'I'
+
+void command_finished_movement(void);
+void command_finished_kicker(void);
+void command_finished_catcher(void);
+
+void setup_command(void);
+void service_command(void);
+
+// Implement these!
+void movement_on_new_command(char cmd, int arg1, int arg2);
+void kicker_on_new_command(char cmd, int arg1);
+void catcher_on_new_command(char cmd, int arg1);
+
+#endif _COMMAND_H_
