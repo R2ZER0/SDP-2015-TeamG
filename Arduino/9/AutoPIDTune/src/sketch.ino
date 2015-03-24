c0ee90837be46b0b49afcc17a23a9119675d640f
Rikki Guy <git@rikkiguy.me.uk>
2015-03-24 22:05:03 +0000

    Now lets try some manual tuning

--- Arduino/9/AutoPIDTune/src/sketch.ino
+++ Arduino/9/AutoPIDTune/src/sketch.ino
@@ -13,8 +13,10 @@
 #define MOTOR_MOTOR3 0
 #define MOTOR_MOTOR4 5
 
-#define ATUNE_STEP 10
-#define ATUNE_NOISE 2
+#define KP 1.0
+#define KI 0.0
+#define KD 0.0
+
 
 void runMotor(int motor, int motor_speed)
 {
@@ -45,9 +47,6 @@ double motor_powers[NUM_MOTORS] = { 0 };
 // Set up PID controllers for each wheel
 PID* wheel_pids[NUM_MOTORS] = { 0 };
 
-
-PID_ATune atune(&(wheel_speeds[0]), &(motor_powers[0]));
-
 /* Motor movement sensors stuff */
 unsigned long next_pid_time = 0L;
 
@@ -62,18 +61,13 @@ void setup() {
     motorAllStop();
     
     for (int i = 0; i < NUM_MOTORS; i++) {
-        PID *pid = new PID(&(wheel_speeds[i]), &(motor_powers[i]), &(desired_speeds[i]), 
-                                0.5, 0.0, 0.25, DIRECT);
+        PID *pid = new PID(&(wheel_speeds[i]), &(motor_powers[i]),
+                           &(desired_speeds[i]), KP, KI, KD, DIRECT);
         pid->SetMode(AUTOMATIC);
         pid->SetSampleTime(sample_time_ms);
         
         wheel_pids[i] = pid;
     }
-    
-    atune.SetNoiseBand(ATUNE_NOISE);
-    atune.SetOutputStep(ATUNE_STEP);
-    atune.SetControlType(1);
-    atune.SetLookbackSec(24);
     
 //     while(Serial.available() > 0) { Serial.read(); }
 //     
@@ -105,39 +99,23 @@ void loop() {
         }
         
         /* Computes new PID expected values for desired speeds and update motors. */
-//         for (int i = 0; i < NUM_MOTORS; i++) {
-//             PID *pid = wheel_pids[i];
-// 
-//             if (desired_speeds[i] < 0) {
-//                 pid->SetOutputLimits(-100, -30);
-//             } else {
-//                 pid->SetOutputLimits(30, 100);
-//             }
-// 
-//             pid->Compute();
-//         }
-        
-        int res = 0;
-        
-        static int count = 0;
-        if(count++ > 10) {
-            res = atune.Runtime();
-        } else {
-            Serial.println("Wait...");
-        }
-            
-        if(res != 0) {
-            Serial.print("Kp="); Serial.print(atune.GetKp());
-            Serial.print("\tKi="); Serial.print(atune.GetKi());
-            Serial.print("\tKd="); Serial.print(atune.GetKd());
-            Serial.println();
+        for (int i = 0; i < NUM_MOTORS; i++) {
+            PID *pid = wheel_pids[i];
+
+            if (desired_speeds[i] < 0) {
+                pid->SetOutputLimits(-100, -30);
+            } else {
+                pid->SetOutputLimits(30, 100);
+            }
+
+            pid->Compute();
         }
 
         // Run motors at these values
         runMotor(MOTOR_MOTOR1, motor_powers[0]);
-//         runMotor(MOTOR_MOTOR2, motor_powers[1]);
-//         runMotor(MOTOR_MOTOR3, motor_powers[2]);
-//         runMotor(MOTOR_MOTOR4, motor_powers[3]);
+        runMotor(MOTOR_MOTOR2, motor_powers[1]);
+        runMotor(MOTOR_MOTOR3, motor_powers[2]);
+        runMotor(MOTOR_MOTOR4, motor_powers[3]);
         
         Serial.print("Motor1");
         Serial.print("\tTarget: ");
