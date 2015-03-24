cf0e75b74d85cdeb61f82deb940427f98ed17569
Thomas Cumming <thomas.cumming18@gmail.com>
2015-03-24 19:00:06 +0000

    Commented new planner module code

--- planning/planner.py
+++ planning/planner.py
@@ -8,6 +8,11 @@ from pyparsing import *
 class Planner:    
 
     def checkTrueConditions(self, world):
+        """An FSM accepts a list of letters from it's alphabet which reflect conditions in the world
+        that are currently true. To build this list, we iterate through the lambdas corresponding to 
+        the letters. For each lambda that results in 'True', we know that the associated letter 
+        represents a true condition in the world. We proceed and add this letter to the accumulating 
+        list"""
         trueCnds = []
         lambdas = self._fsm.getLambdaDic()
         for letter in self._fsm.getAlpha():
@@ -16,20 +21,24 @@ class Planner:
         return trueCnds
 
     def plan(self, world):
+        """The heart of the planner. For every call, we determing a list of FSM letters which hold
+        in the current world state, and provide this to the machine for consumption"""
         truths=self.checkTrueConditions(world)
         self._fsm.consumeInput(truths, self._world, self._robot, self._role)
 
     def __init__(self, world, robot, role, fsmSpecFilePath):
 
-        grammar = createConfigGrammar()
+        
+        grammar = createConfigGrammar()  # Build the grammar used to parse input config files
+
         inFile = open(fsmSpecFilePath, 'r')
         inputText = inFile.read()
-        parseRes = grammar.parseString(inputText)
-    
-        # We're done reading files, so close them
-        inFile.close()
 
-        self._fsm = createFSM(parseRes)
+        parseRes = grammar.parseString(inputText)    # Using PyParsing, initiate a parse run of the input config file
+    
+        inFile.close()      # We're done reading files, so close them
+
+        self._fsm = createFSM(parseRes) # Create the finite state machine object which the planner will use
         self._world = world
         self._robot=robot
         self._role = role
