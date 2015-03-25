03a60a7ec693c6909ee19ef9b9e3b1555714be5e
Thomas Cumming <thomas.cumming18@gmail.com>
2015-03-25 14:52:03 +0000

    Planner now supports multiple finite state machines

--- planning/planner.py
+++ planning/planner.py
@@ -7,15 +7,15 @@ from pyparsing import *
 
 class Planner:    
 
-    def checkTrueConditions(self, world):
+    def checkTrueConditions(self, world,fsm):
         """An FSM accepts a list of letters from it's alphabet which reflect conditions in the world
         that are currently true. To build this list, we iterate through the lambdas corresponding to 
         the letters. For each lambda that results in 'True', we know that the associated letter 
         represents a true condition in the world. We proceed and add this letter to the accumulating 
         list"""
         trueCnds = []
-        lambdas = self._fsm.getLambdaDic()
-        for letter in self._fsm.getAlpha():
+        lambdas = fsm.getLambdaDic()
+        for letter in fsm.getAlpha():
             if letter in lambdas.keys() and lambdas[letter](world) == True:
                 trueCnds.append(letter)
         return trueCnds
@@ -23,11 +23,20 @@ class Planner:
     def plan(self, world):
         """The heart of the planner. For every call, we determing a list of FSM letters which hold
         in the current world state, and provide this to the machine for consumption"""
-        truths=self.checkTrueConditions(world)
-        self._fsm.consumeInput(truths, self._world, self._robot, self._role)
+        print "---------- plan step ----------"
+        print
+        for fsm in self._fsmList:
+            truths=self.checkTrueConditions(world, fsm)    
+            fsm.consumeInput(truths, self._world, self._robot, self._role)
+        print "---------- end plan step ----------"
+        print
 
     def __init__(self, world, robot, role, fsmSpecFilePaths):
 
+        print "Using the following finite state machine spec files:"
+        print
+        for path in fsmSpecFilePaths:
+            print path
         
         grammar = createConfigGrammar()  # Build the grammar used to parse input config files
         self._fsmList = []
