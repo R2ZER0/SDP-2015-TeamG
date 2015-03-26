81d9f7b3c7d1ad3ff692cfe1703d26229b6dda56
Rikki Guy <git@rikkiguy.me.uk>
2015-03-26 14:19:40 +0000

    Added outline of wheels.cpp/h

---
+++ Arduino/9/Movement/src/wheels.cpp
@@ -0,0 +1,10 @@
+/* wheels.cpp - attempting to control the speed of the wheels */
+
+void wheels_set_target_speed(int wheel, double speed)
+{
+    /* TODO */
+}
+
+double wheels_get_speed(int wheel) {
+    /* TODO */
+}
---
+++ Arduino/9/Movement/src/wheels.h
@@ -0,0 +1,8 @@
+/* wheels.h */
+#ifndef _WHEELS_H_
+#define _WHEELS_H_
+
+void wheels_set_target_speed(int wheel, double speed);
+double wheels_get_speed(int wheel);
+
+#endif /* _WHEELS_H_ */
